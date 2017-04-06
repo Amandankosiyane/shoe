51228df2dc582f0fb6ad95362a28b9887726f067
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-04-06 17:33:19 +0200

    Made changes in html and JavaScript

--- shoe.js
+++ shoe.js
@@ -6,37 +6,53 @@ console.log(mySize.value);
      console.log(myBrand.value);
      console.log(ShoeColor.value);
 
-    var theTemplateScript = document.getElementById("template").innerHTML;
-    var theTemplate = Handlebars.compile(theTemplateScript);
-      
-
- function myFunction() {
-    "use strict";
-     
-     
-
-     
-     var shoeList = [
+  var shoeList = [
+        {brand: 'Adidas', color : 'black', price : 2500, in_stock : 15, size: 1},
         {brand: 'Adidas', color : 'black', price : 2500, in_stock : 13, size: 3},
         {brand: 'Adidas', color : 'white', price : 2500, in_stock : 20, size: 5},
         {brand: 'Adidas', color : 'peach', price : 2500, in_stock : 50, size: 6},
         {brand: 'Adidas', color : 'red', price : 2500, in_stock : 5, size: 2},
+        {brand: 'Adidas', color : 'red', price : 2500, in_stock : 5, size: 10},
+        {brand: 'Adidas', color : 'white', price : 2500, in_stock : 5, size: 7},
+        {brand: 'Adidas', color : 'peach', price : 2500, in_stock : 5, size: 4},
+        {brand: 'Adidas', color : 'green', price : 2500, in_stock : 5, size: 9},
+        {brand: 'Adidas', color : 'green', price : 2500, in_stock : 5, size: 8},
+
+        {brand: 'Puma', color : 'black', price : 1900, in_stock : 65, size: 1},
         {brand: 'Puma', color : 'black', price : 1900, in_stock : 30, size: 5},
         {brand: 'Puma', color : 'white', price : 1900, in_stock : 10, size: 3},
-        {brand: 'Puma', color : 'red', price : 1900, in_stock : 20, size: 6},
+        {brand: 'Puma', color : 'red',   price : 1900, in_stock : 20, size: 6},
         {brand: 'Puma', color : 'peach', price : 1900, in_stock : 8, size: 2},
-        {brand: 'Nike', color : 'black', price : 2100, in_stock : 60, size: 2},
-        {brand: 'Nike', color : 'red', price : 2100, in_stock : 20, size: 6},
-        {brand: 'Nike', color : 'peach', price : 2100, in_stock : 5, size: 5},
-        {brand: 'Nike', color : 'white', price : 2100, in_stock : 22, size: 3}];
-        
+        {brand: 'Puma', color : 'white', price : 1900, in_stock : 8, size: 4},
+        {brand: 'Puma', color : 'peach', price : 1900, in_stock : 8, size: 8},
+        {brand: 'Puma', color : 'green', price : 1900, in_stock : 8, size: 10},
+        {brand: 'Puma', color : 'peach', price : 1900, in_stock : 8, size: 7},
+        {brand: 'Puma', color : 'green', price : 1900, in_stock : 8, size: 9},
 
-     
-      var stock = [];
+        {brand: 'Nike', color : 'black', price : 2100, in_stock : 33, size: 1},
+        {brand: 'Nike', color : 'black', price : 2100, in_stock : 60, size: 2},
+        {brand: 'Nike', color : 'red',   price : 2100,   in_stock : 20, size: 6},
+        {brand: 'Nike', color : 'peach', price : 2100, in_stock : 5, size: 5},
+        {brand: 'Nike', color : 'white', price : 2100, in_stock : 22, size: 10},
+        {brand: 'Nike', color : 'red', price : 2100, in_stock : 22, size: 4},
+        {brand: 'Nike', color : 'white', price : 2100, in_stock : 22, size: 8},
+        {brand: 'Nike', color : 'green', price : 2100, in_stock : 22, size: 7},
+        {brand: 'Nike', color : 'peach', price : 2100, in_stock : 22, size: 9},
+        {brand: 'Nike', color : 'green', price : 2100, in_stock : 22, size: 3}];
+
+    var stock = [];
+    var theTemplateScript = document.getElementById("template").innerHTML;
+    var theTemplate = Handlebars.compile(theTemplateScript);
+      
+
+//var filtered = filter(myFunction);
+
+ function myFunction() {
+    "use strict";    
    
     for (var i = 0; i < shoeList.length; i++){
 
-        if(shoeList[i].brand === myBrand.value || shoeList[i].color === ShoeColor.value || shoeList[i].size === Number(mySize.value)){
+        if(shoeList[i].brand === myBrand.value && shoeList[i].color === ShoeColor.value && shoeList[i].size === Number(mySize.value)){
             stock.push(shoeList[i]);
         }
     }
@@ -46,7 +62,3 @@ console.log(mySize.value);
                 });
      userDisplay.innerHTML = showMyStock;
 }; 
-
-
-
-;
